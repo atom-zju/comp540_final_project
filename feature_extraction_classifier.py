68ec7b9c2e777f9c133220d70ebe9af2b6678c9e
atom-zju <zrh4223616@hotmail.com>
2016-04-18 20:10:17 -0500

    added teature extraction classifer

--- feature_extraction_classifier.py
+++ feature_extraction_classifier.py
@@ -3,15 +3,16 @@ import utils
 from sklearn import preprocessing
 from sklearn.cluster import KMeans
 
-def extract_patches(X, stride=1, w_size=3):
+def extract_patches(X, stride=2, w_size=4):
     N = X.shape[0]
     K = (X.shape[1]-w_size)/stride + 1
-    X_patched = np.zeros((N, K, K, w_size*w_size*3), dtype=float)
+    chns = X.shape[-1]
+    X_patched = np.zeros((N, K, K, w_size*w_size*chns), dtype=float)
     # do whitening here
     for idx in range(N):
         for i in range(K):
             for j in range(K):
-                X_patched[idx,i,j,:] = np.reshape(X[idx,i*w_size:(i+1)*w_size,j*w_size:(j+1)*w_size,:],(w_size*w_size*3,))
+                X_patched[idx,i,j,:] = np.reshape(X[idx,i*stride:i*stride+w_size,j*stride:j*stride+w_size,:],(w_size*w_size*chns,))
                 X_patched[idx,i,j,:] = preprocessing.scale(X_patched[idx,i,j,:])
     return X_patched
 
@@ -32,7 +33,7 @@ def transform_features(X_patched,feature_num=400):
     for idx in range(X_patched.shape[0]):
         for i in range(X_patched.shape[1]):
             for j in range(X_patched.shape[2]):
-                feature_label = kmean_classifier.predict(X_patched[idx,i,j,:,:,:])
+                feature_label = kmean_classifier.predict(X_patched[idx,i,j,:])
                 X_transformed[idx,i,j,feature_label] = 1
 
     return X_transformed
@@ -48,5 +49,10 @@ for img_idx in range(N):
 y = cifar10_labels
 
 # create feature patches
-X_patched = extract_patches()
+print "start to do extraction"
+X_patched = extract_patches(X)
+print "start to do transform"
+X_transformed = transform_features(X_patched)
+
+print X_transformed.shape
 
